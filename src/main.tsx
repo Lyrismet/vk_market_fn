6e8580d854d74a438288d0cd6155c94193f7907b
Lyrismet <ana0203tir@gmail.com>
2024-03-16 15:21:43 +0300

    feat: added cart provider to main file

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import {
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import App from './App';
+import {CartProvider} from "./context/CartContext";
+
 
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
 root.render(
     <ConfigProvider>
         <AdaptivityProvider>
+            <CartProvider>
             <App />
+            </CartProvider>
         </AdaptivityProvider>
-    </ConfigProvider>,
+    </ConfigProvider>
 );
